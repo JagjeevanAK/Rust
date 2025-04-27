45343dca9db95217ff687d89fa37d51a58e5b090
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-04-27 21:01:05 +0530

    learnt about error-handling
    Signed-off-by: Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>

---
+++ error-handling/target/debug/deps/error_handling-bf25a61277fea107.d
@@ -0,0 +1,5 @@
+/Users/jagjeevankashid/Developer/Rust/learning/error-handling/target/debug/deps/error_handling-bf25a61277fea107: src/main.rs
+
+/Users/jagjeevankashid/Developer/Rust/learning/error-handling/target/debug/deps/error_handling-bf25a61277fea107.d: src/main.rs
+
+src/main.rs:
---
+++ error-handling/target/debug/error-handling.d
@@ -0,0 +1,1 @@
+/Users/jagjeevankashid/Developer/Rust/learning/error-handling/target/debug/error-handling: /Users/jagjeevankashid/Developer/Rust/learning/error-handling/src/main.rs
